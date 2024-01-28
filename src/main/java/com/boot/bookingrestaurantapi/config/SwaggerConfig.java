7f8de9e1ff13fe9e7a526d108e3c3b5ac231a791
miguel angel cuevas <106960689+ermigue2022@users.noreply.github.com>
2024-01-28 11:41:31 +0100

    ultima actualizacion

--- src/main/java/com/boot/bookingrestaurantapi/config/SwaggerConfig.java
+++ src/main/java/com/boot/bookingrestaurantapi/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.boot.bookingrestaurantapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicate;
+
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -15,8 +18,10 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.boot.bookingrestaurantapi")).paths(PathSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis((Predicate<RequestHandler>) RequestHandlerSelectors.basePackage("com.boot.bookingrestaurantapi"))
+				.paths((Predicate<String>) PathSelectors.any())
 				.build();
 	}
 
